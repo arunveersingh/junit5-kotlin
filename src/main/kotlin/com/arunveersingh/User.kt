e115c760ae7754df0480a0e1fc72dcaa1c9b9ed6
Arunveer Singh <aunveersingh@Aruns-MacBook-Pro.local>
2021-03-07 17:27:57 +0530

    Initial Commit

---
+++ src/main/kotlin/com/arunveersingh/User.kt
@@ -0,0 +1,10 @@
+package com.arunveersingh
+
+class User(val id: Int, val name: String) {
+    var reputation: Int = 0
+        private set
+
+    fun changeReputation(amount: Int){
+        
+    }
+}
---
+++ src/test/kotlin/com/arunveersingh/HelloTest.kt
@@ -0,0 +1,8 @@
+package com.arunveersingh
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class HelloTest {
+
+}
